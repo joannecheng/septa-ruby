6b8cea584d6baaabd854c86f4ac75940ec9a3efc
Joanne Cheng <chengjoanne2@gmail.com>
2012-04-07 18:33:15 -0600

    check for invalid route

--- lib/septa/bus.rb
+++ lib/septa/bus.rb
@@ -6,7 +6,9 @@ module Septa
     def self.bus_locations(bus_number)
       septa_url = "http://www3.septa.org/transitview/bus_route_data/#{bus_number.to_s}"
       resp = Resourceful.get(septa_url)
-      JSON.parse(resp.body)
+      unless resp.body.downcase.include 'invalid route'
+        JSON.parse(resp.body)
+      end
     end
   end
 end
