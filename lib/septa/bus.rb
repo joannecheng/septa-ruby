6d1f0ca3e2834a7ac7134b8dd01df0a5f944aca3
Joanne Cheng <chengjoanne2@gmail.com>
2012-04-10 19:14:21 -0600

    I don't know anything about ruby classes.

--- lib/septa/bus.rb
+++ lib/septa/bus.rb
@@ -3,8 +3,23 @@ require 'json'
 
 module Septa
   class Bus
-    def self.bus_locations(bus_number)
-      septa_url = "http://www3.septa.org/transitview/bus_route_data/#{bus_number.to_s}"
+    attr_accessor :bus_number
+    def initialize(bus_number)
+      @bus_number = bus_number
+    end
+
+    def location
+      locations = {}
+      loc = self.bus_locations()
+      unless loc.nil?
+        loc['bus'].each do |l|
+          locations[l['VehicleID']] = [l['lat'], l['lng']]
+        end
+      end
+    end
+
+    def bus_locations
+      septa_url = "http://www3.septa.org/transitview/bus_route_data/#{@bus_number.to_s}"
       resp = Resourceful.get(septa_url)
       unless resp.body.downcase.include? 'invalid route'
         JSON.parse(resp.body)
--- test/septa_spec.rb
+++ test/septa_spec.rb
@@ -2,12 +2,13 @@ require 'test/spec_helper'
 
 describe 'SEPTA' do
   it 'should return current bus locations' do
-    bus_42 =  Septa::Bus.bus_locations(42)
+    bus_42 = Septa::Bus.new(42)
+    bus_42 = bus_42.bus_locations()
     bus_42["bus"].should_not be nil
   end
 
-  it 'should throw error if invalid route' do
-    bus =  Septa::Bus.bus_locations(1924941)
+  it 'should return nil if invalid route' do
+    bus =  Septa::Bus.new(1924941).bus_locations()
     bus.should be nil
   end
 
