26fdcb5a472d1d2a2558779778d662e1caf27bad
jc <chengjoanne2@gmail.com>
2012-11-29 19:52:43 -0700

    clean up septa gem

--- lib/septa/bus.rb
+++ lib/septa/bus.rb
@@ -4,13 +4,14 @@ require 'json'
 module Septa
   class Bus
     attr_accessor :bus_number
+
     def initialize(bus_number)
       @bus_number = bus_number
     end
 
     def locations
       locations = {}
-      loc = self.bus_locations()
+      loc = bus_locations
       unless loc.nil?
         loc.each do |l|
           locations[l['VehicleID']] = [l['lat'].to_f, l['lng'].to_f]
@@ -20,7 +21,6 @@ module Septa
     end
 
     def format_locations(locations)
-      location = []
       locations.each do |loc| 
         loc['lat'] = loc['lat'].to_f
         loc['lng'] = loc['lng'].to_f
@@ -28,12 +28,13 @@ module Septa
     end
 
     def bus_locations
-      septa_url = "http://www3.septa.org/transitview/bus_route_data/#{@bus_number.to_s}"
+      septa_url = "http://www3.septa.org/TransitView/trips?route=#{@bus_number.to_s}"
       resp = Resourceful.get(septa_url)
       unless resp.body.downcase.include? 'invalid route'
         locations = JSON.parse(resp.body)
-        return self.format_locations(locations['bus'])
+        return format_locations(locations['bus'])
       end
+      []
     end
   end
 end
